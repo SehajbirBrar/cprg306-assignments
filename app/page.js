c5f4665ebe8800df37b54442e3fd0311eb5af225
SehajbirBrar <120230498+SehajbirBrar@users.noreply.github.com>
2023-10-09 20:37:16 -0600

    week4

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Studentinfo />
       <p><Link href="/week2/">Week 2</Link></p>
       <p><Link href="/week3/">Week 3</Link></p>
+      <p><Link href="/week4/">Week 4</Link></p>
       
     </main>
   );
