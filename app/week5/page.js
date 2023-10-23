154e1a03454b16d774288174fae64ec2e99a12f0
SehajbirBrar <120230498+SehajbirBrar@users.noreply.github.com>
2023-10-23 10:58:17 -0600

    wekk5

---
+++ app/week5/page.js
@@ -0,0 +1,16 @@
+
+import ItemList from "./item-list";
+
+
+
+export default function page(){
+    return(
+        <main>
+            <h1>My shopping list</h1>
+
+        <ItemList/>
+        </main>
+
+        
+    );
+}
